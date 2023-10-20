3875ea264349833f6f2bdfa4c222b3a290e55d38
8igmac <8igmac@pm.me>
2023-10-20 22:53:15 +0800

    feat: ask API mock.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,46 @@
 class HomepageController < ApplicationController
+  # Skip CSRF protection for the specific action.
+  skip_before_action :verify_authenticity_token, only: :ask
+
   def index
+  end
+
+  def ask
+    question_asked = ask_params
+    unless question_asked.end_with?("?")
+      question_asked << "?"
+    end
+
+    # TODO: If is cached question, return cached answer.
+
+    # TODO: Otherwise, load embedding and answer qustion with context.
+    docs = "a list of content of each pages"
+    doc_embs = "a list of embeddings of content of each page"
+    result = answer_query_with_context(question_asked, docs, doc_embs)
+    context = result[:context]
+    answer = result[:answer]
+
+    # TODO: Save the question in cache.
+    question = question_asked
+
+    data = {
+      question: question,
+      answer: answer,
+      id: "some id" # TODO: Set primary key in question model.
+    }
+    render json: data
+  end
+
+  private
+  # Do the parameter check using Strong Typeing in Rails.
+  def ask_params
+    params.require(:question)
+  end
+
+  # TODO
+  def answer_query_with_context(question_asked, docs, doc_embs)
+    puts docs
+    puts doc_embs
+    { context: "context", answer: "answer"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "homepage#index"
+
+  post "/ask", to: "homepage#ask"
 end
